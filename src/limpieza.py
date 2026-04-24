ca9d97acfd46f7a177f94ac776ec41140ece79e3
AnamZapa <azapatama1234567@cesde.net>
2026-04-24 18:28:11 -0500

    feat: finalización de la limpieza de datos para estudiantes e inscripciones

--- src/limpieza.py
+++ src/limpieza.py
@@ -4,27 +4,37 @@ from carga import cargaEstudiantes, cargaCursos
 def limpiezaEstudiantes (df_estudiantes): 
     print("Limpieza de datos en el DataFrame de estudiantes:")
     #duplicados
-    df_estudiantes = df_estudiantes.drop_duplicates(subset="Nombre_completo")
+    df_estudiantes = df_estudiantes.drop_duplicates()
     #valores nulos
-    df_estudiantes["Edad"] = df_estudiantes["Edad"].fillna("18")
+    df_estudiantes["Edad"] = df_estudiantes["Edad"].fillna("16")
+    df_estudiantes["Genero"] = df_estudiantes["Genero"].replace("", np.nan)
     df_estudiantes["Genero"] = df_estudiantes["Genero"].fillna("Prefiere no decir")
+    df_estudiantes["Correo_electronico"] = df_estudiantes["Correo_electronico"].replace("", np.nan)
     df_estudiantes["Correo_electronico"] = df_estudiantes["Correo_electronico"].fillna("sincorreo@correo.com")
+    df_estudiantes["Telefono"] = df_estudiantes["Telefono"].replace("", np.nan)
     df_estudiantes["Telefono"] = df_estudiantes["Telefono"].fillna("0000000000")
+    df_estudiantes["Carreer_aplicada"] = df_estudiantes["CarreraAplicada"].replace("", np.nan)
     df_estudiantes["Carrera_aplicada"] = df_estudiantes["CarreraAplicada"].fillna("Desconocida")
     df_estudiantes["Estrato_socioeconomico"] = df_estudiantes["Estrato_socioeconomico"].fillna("0")
     df_estudiantes["Fecha_nacimiento"] = df_estudiantes["Fecha_nacimiento"].fillna("1900-01-01")
     #estandarizacion de texto, eliminacion de espacios y conversion a formato title case
     df_estudiantes['Nombre_completo'] = df_estudiantes['Nombre_completo'].str.strip().str.title()
+    df_estudiantes["Nombre_completo"] = df_estudiantes["Nombre_completo"].str.title()
+    df_estudiantes["Genero"] = df_estudiantes["Genero"].str.strip().str.title()
     df_estudiantes['Correo_electronico'] = df_estudiantes['Correo_electronico'].str.strip().str.title()
+    df_estudiantes["Correo_electronico"] = df_estudiantes["Correo_electronico"].str.lower()
+    df_estudiantes["Telefono"] = df_estudiantes["Telefono"].str.strip()
+    df_estudiantes["Carrera_aplicada"] = df_estudiantes["Carrera_aplicada"].str.strip().str.title()
     df_estudiantes["Ciudad_origen"] = df_estudiantes["Ciudad_origen"].str.strip().str.title()
-    #LISTA ESTRICTA DE GENEROS VALIDOS PARA EL CAMPO GENERO
-    Genero = ["Masculino", "Femenino", "Prefiere no decir"]
-    mascara_infractores = ~df_estudiantes["Genero"].isin(Genero)
-    errores_genero = df_estudiantes[mascara_infractores]
-    traduccion_genero = {"M": "Masculino", "F": "Femenino", "FEMENINO": "Femenino", "masculino": "Masculino", 
-                         "N/A": "Prefiere no decir", "Hombre": "Masculino", "mujer": "Femenino"}
+    df_estudiantes["Ciudad_origen"] = df_estudiantes["Ciudad_origen"].str.title()
+    df_estudiantes["Fecha_nacimiento"] = df_estudiantes["Fecha_nacimiento"].str.strip() 
+    #Genero
+    traduccion_genero = {"M": "Masculino", "m": "Masculino", "F": "Femenino", "FEMENINO": "Femenino", "masculino": "Masculino", 
+                         "N/A": "Prefiere no decir", "Hombre": "Masculino", "mujer": "Femenino","f": "Femenino"}
     df_estudiantes["Genero"] = df_estudiantes["Genero"].replace(traduccion_genero)
-    #LISTA ESTRICTA DE GENEROS VALIDOS PARA EL CAMPO CARRERA APLICADA
+    generos_validos = ["Masculino", "Femenino", "Prefiere no decir"]
+    df_estudiantes.loc[~df_estudiantes["Genero"].isin(generos_validos), "Genero"] = "Prefiere no decir"
+    #Carrera aplicada
     Carrera_aplicada = ["Ingeniería Civil", "Contaduría Pública", "Administración de Empresas", "Arquitectura", "Medicina", 
                          "ingeniería industrial", "Ingeniería de Sistemas", "Diseño Gráfico", "Derecho", "Psicología", "Desconocida"]
     mascara_infractores_carrera = ~df_estudiantes["Carrera_aplicada"].isin(Carrera_aplicada)
@@ -35,13 +45,18 @@ def limpiezaEstudiantes (df_estudiantes):
                          "Derecho": "Derecho", "PSICOLOGIA": "Psicología", "DECONOCIDA ": "Desconocida "}
     df_estudiantes["Carrera_aplicada"] = df_estudiantes["Carrera_aplicada"].replace(traduccion_carrera)
     #MANEJO DE ERRORES EN EL CAMPO TELEFONO, ELIMINACION DE CARACTERES NO NUMERICOS Y CONVERSION A STRING
-    df["Telefono"] = df["Telefono"].astype(str).str.replace(r"\D", "", regex=True)
+    df_estudiantes["Telefono"] = df_estudiantes["Telefono"].astype(str).str.replace(r"\D", "", regex=True)
+    df_estudiantes.loc[df_estudiantes["Telefono"].str.len()<10, "Telefono"] = "0000000000"
     #CONVERSION DE EDAD A NUMERICO, CON MANEJO DE ERRORES Y FILTRADO DE EDADES INCOHERENTES
     df_estudiantes["Edad"] = pd.to_numeric(df_estudiantes["Edad"], errors="coerce") 
     df_estudiantes.loc[(df_estudiantes["Edad"] < 16) | (df_estudiantes["Edad"] > 80), "Edad"] = None
     #CONVERSION DE FECHA DE NACIMIENTO A FORMATO DE FECHA, CON MANEJO DE ERRORES
     df_estudiantes["Fecha_nacimiento"] = pd.to_datetime(df_estudiantes["Fecha_nacimiento"], errors="coerce", dayfirst=True)
-
+    #estrato
+    traduccion_estrato = {"uno": 1, "DOS": 2, "N/A": 0}
+    df_estudiantes["Estrato_socioeconomico"] = df_estudiantes["Estrato_socioeconomico"].replace(traduccion_estrato)
+    df_estudiantes["Estrato_socioeconomico"] = pd.to_numeric(df_estudiantes["Estrato_socioeconomico"], errors="coerce")
+    df_estudiantes["Estrato_socioeconomico"] = df_estudiantes["Estrato_socioeconomico"].fillna(0)
     return df_estudiantes
 
 def limpiezaInscripciones (df_inscripciones):
@@ -93,6 +108,10 @@ def limpiezaInscripciones (df_inscripciones):
     df_inscripciones["Semestre_inscripcion"] = pd.to_numeric(df_inscripciones["Semestre_inscripcion"], errors="coerce")
     #fecha de inscripcion, conversion a formato de fecha y manejo de errores
     df_inscripciones["Fecha_inscripcion"] = pd.to_datetime(df_inscripciones["Fecha_inscripcion"], errors="coerce", dayfirst=True)
-
-
+    #nulos
+    df_inscripciones["Asesor_asignado"] = df_inscripciones["Asesor_asignado"].replace("", "Sin Asesor")
+    df_inscripciones["Asesor_asignado"] = df_inscripciones["Asesor_asignado"].fillna("Sin Asesor")
+    df_inscripciones["Valor_matricula"] = df_inscripciones["Valor_matricula"].fillna(df_inscripciones["Valor_matricula"].median())
+    df_inscripciones["Creditos_programa"] = df_inscripciones["Creditos_programa"].fillna(df_inscripciones["Creditos_programa"].median())
+    df_inscripciones["Semestre_inscripcion"] = df_inscripciones["Semestre_inscripcion"].fillna(df_inscripciones["Semestre_inscripcion"].median())
     return df_inscripciones
